10567e2fdc7fb9daeec75fad1aabc77899abf05a
kwonkeonhyeong <rjsgud19566@gmail.com>
2024-03-15 11:29:40 +0900

    add Hi.java

---
+++ src/Hi.java
@@ -0,0 +1,8 @@
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("i'm so happy");
+        System.out.println("wow");
+        System.out.println("frist commit");
+    }
+}
